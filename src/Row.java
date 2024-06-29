64d02987a3ffd8903d0a8e39ae5132d6ee5f8fb9
labib10849 <154999586+labib10849@users.noreply.github.com>
2024-06-29 22:48:01 +0600

    Add files via upload

---
+++ src/Row.java
@@ -0,0 +1,16 @@
+public class Row 
+{
+    private static int row;
+    public static int getRow()
+    {
+        return row;
+    }
+    public static void setRow()
+    {
+        row++;
+    }
+    public static void setRow( int temp)
+    {
+        row= temp;
+    }
+}
